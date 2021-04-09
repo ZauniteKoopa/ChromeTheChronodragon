f0435aa2a7b69cb425a88d334b242c239296eb28
Anthony Hoang <anthonyhoang3074@gmail.com>
2021-04-09 11:23:52 -0500

    Commit that has everything

---
+++ Assets/Scripts/UI/ManaDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManaDisplay : MonoBehaviour {
+
+    private PlayerStatus status;
+    public GameObject player;
+    public Text manaText;
+
+    // Use this for initialization
+    void Start()
+    {
+        status = player.GetComponent<PlayerStatus>();
+        manaText.text = "Mana: " + status.mana;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        manaText.text = "Mana: " + status.mana;
+    }
+}
